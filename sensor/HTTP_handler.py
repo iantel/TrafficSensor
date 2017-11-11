f19f5165aae57e877c1caa170e7964cf39936924
Ian <ian.dn.tran@gmail.com>
2017-11-11 16:57:41 -0500

    matching http field with post call from sensor

--- sensor/HTTP_handler.py
+++ sensor/HTTP_handler.py
@@ -1,19 +1,20 @@
 import requests
 
 class HTTP_handler:
-    #   Anytime the sensor picks up interesting activity (entering or leaving)
-    #   call this method to send a POST call to backend and update room population count accordingly
+    
     __queue = []
     url = ""
 
     def __init__(self):
         print("Set backend url here.")
 
-
+    #   Anytime the sensor picks up interesting activity (entering or leaving)
+    #   call this method to send a POST call to backend and update room population count accordingly
     @staticmethod
     def post_event(event_type):
+        event_str = 'up' if event_type == 1 else 'down'
         # timeout is 5 seconds at which point we store it on a queue to POST at a later point in time.
         try:
-            response = requests.post("http://httpbin.org/post", data={'event':event_type}, timeout=3.0)
+            response = requests.post("http://httpbin.org/post", data={'event':event_str}, timeout=3.0)
         except requests.exceptions.Timeout:
-            __queue.append(event_type)
+            __queue.append(event_type) # save failed update on a queue and attempt to post it later
